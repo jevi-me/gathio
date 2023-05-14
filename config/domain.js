10b36fae188036bd3b77a1d0edea7986175559bb
Jevi <42013557+jevi-me@users.noreply.github.com>
2023-05-14 10:33:28 -0400

    Update domain.js

--- config/domain.js
+++ config/domain.js
@@ -1,6 +1,6 @@
 module.exports = {
     // Your domain goes here. If there is a port it should be 'domain:port', but otherwise just 'domain'
-    'domain' : 'events.jevi.me' ,
+    'domain' : 'events.jevi.space' ,
     'port': '3000',
     'email': 'info@events.jevi.space',
     'mailService': 'nodemailer', // Which mail service to use to send emails to attendees. Options are 'nodemailer' or 'sendgrid'. Configure settings for the mail service in config/api.js.z
